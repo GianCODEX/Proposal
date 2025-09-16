a96d43ec767f6afe49e3ee3953cefe97573b0120
GianCODEX <gianrossbcorpuz@gmail.com>
2025-09-16 20:15:02 +0800

    Update 0.5 Proposal.c
    Added const name, and data types

--- Proposal.c
+++ Proposal.c
@@ -2,15 +2,18 @@
 #include <stdbool.h>
 #include <string.h>
  
+ 
+void studentChecker(bool isStudent, const char *name);
+ 
 int main(){
     FILE *fptr;
-    bool isStudent = true;
+    bool isStudent = false;
+    char choiceYorN[5] = "";
     char name[50] = "";
     char location[50] = "";
     int age = 0;
     int choice;
 
-    printf("Create a Student Account... \n");
     printf("Create a Student Account... \n");
     printf("1. Continue\n");
     printf("2. Exit\n");
@@ -19,7 +22,7 @@ int main(){
     getchar(); 
     switch(choice){
         case 1:
-            printf("Enter your name: ");
+            printf("Enter your full name: ");
             fgets(name, sizeof(name), stdin);
             if (name[strlen(name) - 1] == '\n') name[strlen(name) - 1] = '\0';
             while(strlen(name) < 10){
@@ -38,7 +41,15 @@ int main(){
             printf("Please type your address: ");
             fgets(location, sizeof(location), stdin);
             if (location[strlen(location) - 1] == '\n') location[strlen(location) - 1] = '\0';
+            
+            printf("Are you a Student of NU Lipa: (YES or NO)");
+            fgets(choiceYorN, sizeof(choiceYorN), stdin);
+            choiceYorN[strcspn(choiceYorN, "\n")] = '\0';
+            if (strcasecmp(choiceYorN, "yes") == 0) {
+            isStudent = true;
+            }
             break;
+            
         case 2:
             printf("You have successfully exited.\n");
             return 0;
@@ -46,10 +57,12 @@ int main(){
             printf("Invalid choice.\n");
             return 1;
     }
+
     printf("=================================\n");
     printf("%s\n", name);
     printf("%d\n", age);
     printf("%s\n", location);
+    studentChecker(isStudent, name);
     
     fptr = fopen("C:/Users/ACER/Desktop/userdata.txt", "w");
     if (fptr == NULL) {
@@ -63,4 +76,13 @@ int main(){
     fclose(fptr);
     printf("Text has been saved to userdata.txt\n");
     return 0;
+
+}
+
+void studentChecker(bool isStudent, const char *name) {
+    if (isStudent) {
+        printf("%s is a student of NU Lipa\n", name);
+    } else {
+        printf("%s is not enrolled in NU Lipa\n", name);
+    }
 }
